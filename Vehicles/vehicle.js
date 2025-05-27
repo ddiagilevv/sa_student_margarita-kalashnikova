4f66a04695d9ce3a86ff67b05f4f739e42a24b1f
java-strip <rita.kalash@icloud.com>
2025-05-27 17:45:10 +0400

    27-05. Practice. Vehicles.

--- Vehicles/vehicle.js
+++ Vehicles/vehicle.js
@@ -13,17 +13,102 @@
 //обработать исключения (exceptions.md in sa repository)
 
 class Vehicle {
-    constructor(id, brand) {
+    constructor(id, brand, color, rentalPrice, purchasePrice) {
+        this.id = id;
+        this.brand = brand;
+        this.color = color;
+        this.rentalPrice = rentalPrice;
+        this.purchasePrice = purchasePrice;
+        this.available = true;
 
     }
 
     changeAvailable() {
+        this.available = !this.available;
+    }
 
+    setAvailability(status) {
+        this.available = status;
+    }
+
+    getRentalPrice() {
+        return this.rentalPrice;
+    }
+
+    getPurchasePrice() {
+        return this.purchasePrice;
+    }
+
+    getColor() {
+        return this.color;
+    }
+
+    getBrand() {
+        return this.brand;
     }
 }
 
+
 class Car extends Vehicle {
-    constructor(id, brand)
+    constructor(id, brand, color, rentalPrice, purchasePrice, year, model) {
+        super(id, brand, color, rentalPrice, purchasePrice);
+        this.year = year;
+        this.model = model;
+    }
+}
+
+class Bike extends Vehicle {
+    constructor(id, brand, color, rentalPrice, purchasePrice, size, useType) {
+        super(id, brand, color, rentalPrice, purchasePrice);
+        this.size = size;
+        this.useType = useType;
+    }
+}
+
+class Plane extends Vehicle {
+    constructor(id, brand, color, rentalPrice, purchasePrice, size, _class) {
+        super(id, brand, color, rentalPrice, purchasePrice);
+        this.size = size;
+        this._class = _class;
+    }
 }
 
 
+class Dealer {
+    constructor(){
+        this.vehicles = new Map();
+    }
+
+    addVehicle(vehicle) {
+        if (this.vehicles.has(vehicle.id)) {
+            throw new Error("ID уже существует");
+        }
+        this.vehicles.set(vehicle.id, vehicle);
+    }
+
+    rentVehicle(id) {
+        const vehicle = this.vehicles.get(id)
+        if (!vehicle) {
+            throw new Error("Транспорт не найден");
+        }
+        if (!vehicle.available) {
+            throw new Error("Транспорт не доступен");
+        }
+        vehicle.setAvailability(false);
+        return vehicle;
+    }
+
+    purchaseVehicle(id) {
+        const vehicle = this.vehicles.get(id)
+        if (!vehicle) {
+            throw new Error("Транспорт не найден");
+        }
+        this.vehicles.delete(id);
+        return vehicle;
+    }
+
+    listAvailableVehicles() {
+        return Array.from(this.vehicles.values()).filter(v => v.available);
+    }
+
+}
